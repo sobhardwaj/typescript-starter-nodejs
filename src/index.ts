e2616bdd4f27db4954f905b56dcaa77223dbcd8c
sobhardwaj <siddharathbhardwaj@gmail.com>
2020-05-06 06:35:29 +0530

    setup as typscript server

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { PORT } from './config/constants';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send({ message: 'endpoint working' });
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
